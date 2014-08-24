64a20090059f011bccdd224552b72d3fc7aab4b6
Dominyk Tiller <dom_t4@yahoo.co.uk>
2014-08-24 04:26:19 +0100

    Initial Commit
    Commit Number 1.

---
+++ OSX_Node_Removal/README.md
@@ -0,0 +1,14 @@
+terminatenode.sh
+=======
+
+You will need to run ``` chmod +x /path/to/terminatenode.sh ``` for this script to execute.
+
+This is an *extremely* methodical script to **completely** purge Node & npm and every file connected to those two environments. The script could be compacted but it is deliberately exploded to enable people to see exactly what the script is doing, and for clarity if any errors are kicked out.
+
+I like to write scripts where people can actually see what's going on. I find it helpful for education and clarity, even if it is more clunky than the traditional couple of compact lines.
+
+This script will remove **everything** about Node & npm, including any npm modules you currently have installed.
+
+This script is only tested on OS X, but theoretically it may well work on Linux as well.
+
+It's MIT Licensed, so feel free to do whatever you wish with it.
---
+++ OSX_Node_Removal/terminatenode.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+read -p "This will completely erase all traces of Node/npm from your system. You will be asked to enter your password for sudo. Proceed? (y/n)" -n 1 -r
+echo    # blank echo to prevent errors
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+	brew remove node
+	brew cleanup
+	brew prune
+	echo "If it says a file/folder can't be found, this is a good thing!"
+	sudo rm -r ~/.node
+	sudo rm -r ~/.npm
+	sudo rm -r ~/.node-gyp
+	sudo rm -r ~/.npmrc
+	sudo rm -r ~/node_modules
+	sudo rm /usr/local/bin/node
+	sudo rm /usr/local/bin/npm
+	sudo rm -r /usr/local/include/node
+	sudo rm -r /usr/local/lib/node_modules
+	sudo rm -r /usr/local/lib/dtrace
+	sudo rm /usr/local/share/man/man3/npm-*
+	sudo rm /usr/local/share/man/man3/npm.3
+	sudo rm /usr/local/share/man/man1/node.1
+	sudo rm /usr/local/share/man/man1/npm-*
+	sudo rm /usr/local/share/man/man1/npm.1
+	sudo rm /usr/local/share/man/man5/npm-*
+	sudo rm /usr/local/share/man/man5/npmrc.5
+	sudo rm /usr/local/share/man/man7/npm-*
+	sudo rm /usr/local/share/man/man7/removing-npm.7
+	find -L /usr/local/bin -type l -exec rm -i {} +
+	find -L /usr/local/include -type l -exec rm -i {} +
+	echo "You should now do 'chown YOURUSENAME /usr/local'"
+fi
