6020933faaa847d181c8c3b4518edd68c202b928
Dominyk Tiller <dominyktiller@gmail.com>
2015-06-17 21:56:37 +0100

    terminatenode.sh: updates & improvements
    * Updates the directories/files removed to cover the most recent Node versions
    * Suppressed the unclear/worrying messages about "No such file found".
    * Always run from HOME directory to more accurately pick up local npm modules
    * Push the chown command onto a unique echo for clarity

--- OSX_Node_Removal/terminatenode.sh
+++ OSX_Node_Removal/terminatenode.sh
@@ -4,35 +4,38 @@ echo    # blank echo to prevent random errors
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     echo "We'll make a backup of your npm installed list so you can reference it later if desired."
+    cd $HOME
     npm ls -g --depth=0 >> npm_global.txt
     npm ls --depth=0 >> npm_non_global.txt
     echo "Lists have been created! You'll find them in whatever directory you are currently cd'ed into."
     brew remove node
     brew cleanup
     brew prune
-    echo "If it says a file/folder can't be found, this is a good thing!"
-    sudo rm -r ~/.node
-    sudo rm -r ~/.npm
-    sudo rm -r ~/.node-gyp
-    sudo rm -r ~/.npmrc
-    sudo rm -r ~/node_modules
-    sudo rm -r ~/Library/Logs/Homebrew/node
-    sudo rm /usr/local/bin/node
-    sudo rm /usr/local/bin/npm
-    sudo rm -r /usr/local/include/node
-    sudo rm -r /usr/local/lib/node_modules
-    sudo rm -r /usr/local/lib/dtrace
-    sudo rm /usr/local/share/man/man3/npm-*
-    sudo rm /usr/local/share/man/man3/npm.3
-    sudo rm /usr/local/share/man/man1/node.1
-    sudo rm /usr/local/share/man/man1/npm-*
-    sudo rm /usr/local/share/man/man1/npm.1
-    sudo rm /usr/local/share/man/man5/npm-*
-    sudo rm /usr/local/share/man/man5/npmrc.5
-    sudo rm /usr/local/share/man/man7/npm-*
-    sudo rm /usr/local/share/man/man7/removing-npm.7
+    sudo rm -rf ~/.node
+    sudo rm -rf ~/.npm
+    sudo rm -rf ~/.node-gyp
+    sudo rm -rf ~/.npmrc
+    sudo rm -rf ~/node_modules
+    sudo rm -rf ~/Library/Logs/Homebrew/node
+    sudo rm -rf /usr/local/include/node
+    sudo rm -rf /usr/local/lib/node_modules
+    sudo rm -rf /usr/local/lib/dtrace
+    sudo rm -rf /usr/local/share/systemtap
+    sudo rm -f /usr/local/bin/node
+    sudo rm -f /usr/local/bin/npm
+    sudo rm -f /usr/local/etc/bash_completion.d/npm
+    sudo rm -f /usr/local/share/man/man3/npm-*
+    sudo rm -f /usr/local/share/man/man3/npm.3
+    sudo rm -f /usr/local/share/man/man1/node.1
+    sudo rm -f /usr/local/share/man/man1/npm-*
+    sudo rm -f /usr/local/share/man/man1/npm.1
+    sudo rm -f /usr/local/share/man/man5/npm-*
+    sudo rm -f /usr/local/share/man/man5/npmrc.5
+    sudo rm -f /usr/local/share/man/man7/npm-*
+    sudo rm -f /usr/local/share/man/man7/removing-npm.7
     find -L /usr/local/bin -type l -exec rm -i {} +
     find -L /usr/local/include -type l -exec rm -i {} +
     find -L /usr/local/lib -type l -exec rm -i {} +
-    echo "You may also need to do 'sudo chown -R `whoami` /usr/local'"
+    echo "You may also need to do:"
+    echo "sudo chown -R `whoami` /usr/local"
 fi
